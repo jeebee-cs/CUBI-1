4554f69f0dc71b5ada8fd9f51e87d8d3cf5bb64c
gabby <24212395+n0uille@users.noreply.github.com>
2024-03-17 22:49:26 -0400

    Integrate Wwise into Unity project

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AI_DREAM_GET = 4170308110U;
+        static const AkUniqueID AI_FS = 3436755455U;
+        static const AkUniqueID AI_JUMP = 77128254U;
+        static const AkUniqueID BLOCK_PUSH = 2060495175U;
+        static const AkUniqueID DREAM_AMBIENCE = 3982734893U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+        static const AkUniqueID MUSIC_STOP = 3227181061U;
+        static const AkUniqueID PLAYER_DREAM_GET = 740186403U;
+        static const AkUniqueID PLAYER_FS = 4276063004U;
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
