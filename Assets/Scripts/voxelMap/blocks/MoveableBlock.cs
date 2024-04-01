22a7fadfa695c368f20d90d23e33faa436f67329
Neokoldstudio <nightmare.vid@gmail.com>
2024-04-01 19:23:11 -0400

    block pushing has been transfered to the player. moving blocks now have the new displacementSpeed parameter

--- Assets/Scripts/voxelMap/blocks/MoveableBlock.cs
+++ Assets/Scripts/voxelMap/blocks/MoveableBlock.cs
@@ -1,59 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using static ABlock;
 
 public class MoveableBlock : ABlock
 {
     public bool IsMoving { get; set; }
-
-    private bool IsMovable = false;
     private Vector3  directionToPush;
     [SerializeField] LayerMask _layerCollision;
     [SerializeField] LayerMask _layerBlock;
 
-    void Update()
+    [SerializeField, Range(0f, 1f)]
+	private float deplacementTime = 0.5f;
+
+    public void MoveBlock(Vector3 otherPos)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            MoveBlock();
-        }
+
+        Vector3 directionToOther = otherPos - transform.position;
+
+        if (Mathf.Abs(directionToOther.y) > .5f) return;
+
+        directionToOther = directionToOther.normalized;
+
+        Debug.Log(directionToOther);
+        directionToOther.x = Mathf.Round(directionToOther.x * .6f);
+        directionToOther.z = Mathf.Round(directionToOther.z * .6f);
+        directionToOther.y = 0;
+
+        directionToPush = directionToOther * -1;
+
+        if (Physics.Raycast(transform.position, directionToPush, 1f, _layerBlock)) return;
+
+        //AkSoundEngine.PostEvent("Block_Push", this.gameObject);
+
+        StartCoroutine(SmoothLerp(transform.position + directionToPush, deplacementTime));
     }
 
-    private void OnCollisionEnter(Collision other)
+    private IEnumerator SmoothLerp (Vector3 newPosition, float time)
     {
-        if (_layerCollision == (_layerCollision | (1 << other.gameObject.layer)))
-        {
-            Vector3 directionToOther = other.gameObject.transform.position - transform.position;
+            Vector3 startingPos  = transform.position;
 
-            if (Mathf.Abs(directionToOther.y) > .5f) return;
+            float elapsedTime = 0;
 
-            directionToOther = directionToOther.normalized;
-
-            Debug.Log(directionToOther);
-            directionToOther.x = Mathf.Round(directionToOther.x * .6f);
-            directionToOther.z = Mathf.Round(directionToOther.z * .6f);
-            directionToOther.y = 0;
-
-            directionToPush = directionToOther * -1;
-
-            if (Physics.Raycast(transform.position, directionToPush, 1f, _layerBlock)) return;
-
-            IsMovable = true;
-            //AkSoundEngine.PostEvent("Block_Push", this.gameObject);
-        }
-    }
-
-    private void OnCollisionExit(Collision other){
-        IsMovable = false;
-    }
-
-    void MoveBlock()
-    {
-        if(IsMovable){
-            transform.position += directionToPush;
-            IsMovable = false;
-        }
+            while (elapsedTime < time)
+            {
+                transform.position = Vector3.Lerp(startingPos, newPosition, (elapsedTime / time));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
     }
 }
